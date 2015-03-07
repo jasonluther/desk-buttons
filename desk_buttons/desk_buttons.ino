b78d71141b18f018c2f1708c960661be9653b11c
Jason Luther <jason@ixid.net>
2015-03-07 12:34:22 -0500

    Set LED PWM values over Serial. Fix RGB pin assignment.

--- desk_buttons/desk_buttons.ino
+++ desk_buttons/desk_buttons.ino
@@ -1,16 +1,16 @@
 // Desk Buttons!
 // Jason Luther <jason@ixid.net>
 
-#define LED_RED 9
+#define LED_RED 11
 #define LED_GREEN 10
-#define LED_BLUE 11
+#define LED_BLUE 9
 #define BUTTON_RED 12
 #define LED_BUTTON_RED 13
 #define BUTTON_SECRET 7
 #define BUTTON_YELLOW 2
 #define BUTTON_WHITE 4
 
-#define BOUNCE_TIME 100
+#define BOUNCE_TIME 50
 
 unsigned char led_rgb[3];
 unsigned char led_button_red = 0;
@@ -55,10 +55,6 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED_RED, HIGH);
-  digitalWrite(LED_GREEN, HIGH);
-  digitalWrite(LED_BLUE, HIGH);
-  digitalWrite(LED_BUTTON_RED, HIGH);
 
   for (int i = 0; i < NUM_BUTTONS; i++) {
     int value = digitalRead(button_pin[i]); 
@@ -75,10 +71,43 @@ void loop() {
 
   for (int i = 0; i < NUM_BUTTONS; i++) {
     if (button_send[i] != SEND_NO) {
+      Serial.print("%");
       Serial.print(button_name[i]); Serial.print(": "); Serial.println(button_send[i]);
       button_send[i] = SEND_NO;
     }
   }
-
+  
+  while (Serial.available() > 0) {
+    int inbyte = Serial.read();
+    if (inbyte == '@') {
+      char color = Serial.read();
+      Serial.read(); // should be '=' 
+      int pwm_val = Serial.parseInt();
+      switch(color) {
+        case 'R':
+          led_rgb[0] = pwm_val;
+          break;
+        case 'G':
+          led_rgb[1] = pwm_val;
+          break;
+        case 'B':
+          led_rgb[2] = pwm_val;
+          break;
+        case 'L':
+          led_button_red = pwm_val;
+          break;
+      }
+      Serial.print("#");
+      Serial.print(color);
+      Serial.print(":");
+      Serial.println(pwm_val);
+    }
+  }
+  
+  analogWrite(LED_RED, led_rgb[0]);
+  analogWrite(LED_GREEN, led_rgb[1]);
+  analogWrite(LED_BLUE, led_rgb[2]);
+  analogWrite(LED_BUTTON_RED, led_button_red);
+  
   delay(10);
 }
